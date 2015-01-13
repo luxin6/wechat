a707372e6aa5adef3b9a55fb9c446e26c1346324
Garry <garrydzeng@users.noreply.github.com>
2015-01-13 08:58:16 +0800

    reformat code & bugfix

--- src/exception.php
+++ src/exception.php
@@ -1,13 +1,13 @@
 <?php
 namespace wechat {
-	class exception extends \exception {
-		const SERVER_BUSY = -1;
-		const INVALID_ENCRYPTION = -40007;
-		const MALFORMED_JSON = 47001;
-		const MALFORMED_ACCESS_TOKEN = 40001;
-		const MISSING_CORPID = 41002;
-		const MISSING_CORPSECRET = 41004;
-		const INVALID_CORPID = 40013;
-		const INVALID_CORPSECRET = 40001;
-	}
+  class exception extends \exception {
+    const SERVER_BUSY = -1;
+    const INVALID_ENCRYPTION = -40007;
+    const MALFORMED_JSON = 47001;
+    const MALFORMED_ACCESS_TOKEN = 40001;
+    const MISSING_CORPID = 41002;
+    const MISSING_CORPSECRET = 41004;
+    const INVALID_CORPID = 40013;
+    const INVALID_CORPSECRET = 40001;
+  }
 }
